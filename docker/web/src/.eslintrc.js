1d7c8ecf9ed87b708308fbcc9a0cc0b3389b28b5
yyukigame <yyukigame@penguin>
2021-07-11 22:35:42 +0900

    config eslint

--- docker/web/src/.eslintrc.js
+++ docker/web/src/.eslintrc.js
@@ -6,18 +6,18 @@ module.exports = {
   },
   extends: [
     '@nuxtjs/eslint-config-typescript',
-    'prettier',
-    //    'plugin:prettier/recommended',
+    // 'prettier',
+    'plugin:prettier/recommended',
     'plugin:nuxt/recommended',
   ],
   plugins: [],
   // add your custom rules here
-  "rules": {
+  /*  "rules": {
     "semi": ["error", "always"],
     "semi-spacing": ["error", {"after": true, "before": false}],
     "semi-style": ["error", "last"],
     "no-extra-semi": "error",
     "no-unexpected-multiline": "error",
     "no-unreachable": "error"
-  }
+  } */
 };
